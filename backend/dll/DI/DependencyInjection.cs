99f288633b056a5fb718b6642deb577c24d9e0a4
Lucas Neves <lucasbarbozaneves@gmail.com>
2023-03-28 19:07:35 -0300

    Criando o DependecyInjection, permitindo a injeção de dependências através da classe de inicialização do aplicativo.

---
+++ backend/dll/DI/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dll.DI
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraStructureDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var provider = services.AddDbContext<DbContext>(options => 
+                options.UseSqlServer(configuration.GetConnectionString("DbConnection")))
+                .BuildServiceProvider();
+            
+            var context = provider.GetRequiredService<DbContext>();
+            DbInitializer.Initialize(context);
+
+            return services;
+        }
+    }
+}
