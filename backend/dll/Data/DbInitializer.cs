6b67bc0ffa1339fe8d37571322037813f099001d
Lucas Neves <lucasbarbozaneves@gmail.com>
2023-03-28 20:34:41 -0300

    Criando classe para inicializar e popular um banco de dados com dados iniciais quando a aplicação é executada pela primeira vez.

---
+++ backend/dll/Data/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dll.Data
+{
+    internal class DbInitializer
+    {
+        public static void Initialize(CareerMapContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
