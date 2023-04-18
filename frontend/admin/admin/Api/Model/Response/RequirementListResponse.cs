c9583e0a4270bb7c20870a5538dd671587d58a27
Marcio Fernandes <marcio.fernandes@atos.net>
2023-04-17 21:58:49 -0300

    copiando API mobile para admin

---
+++ frontend/admin/admin/Api/Model/Response/RequirementListResponse.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace admin.Api.Model.Response
+{
+    public class RequirementResponse
+    {
+        [JsonProperty("groupName")]
+        public string GroupName { get; set; }
+
+        [JsonProperty("requirement")]
+        public RequirementInfo RequirementInfo { get; set; }
+    }
+
+    //public class Requirement2
+    public class RequirementInfo
+    {
+        [JsonProperty("requirementId")]
+        public int RequirementId { get; set; }
+
+        [JsonProperty("requirementName")]
+        public string RequirementName { get; set; }
+    }
+
+    public class RequirementListResponse
+    {
+        [JsonProperty("careerMap")]
+        public CareerMapResponse CareerMap { get; set; }
+
+        [JsonProperty("companyPosition")]
+        public CompanyPositionInfo CompanyPositionInfo { get; set; }
+
+        [JsonProperty("requirements")]
+        public List<RequirementResponse> Requirements { get; set; }
+    }
+}
---
+++ frontend/admin/admin/Api/Service/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+
+namespace admin.Api.Service
+{
+    public class BaseService
+    {
+        protected string baseUrl = "https://localhost:7149/api/";
+        protected HttpClient httpClient;
+        protected string endpoint;
+        public BaseService() 
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+            };
+            httpClient = new HttpClient(clientHandler);
+        }
+    }
+}
