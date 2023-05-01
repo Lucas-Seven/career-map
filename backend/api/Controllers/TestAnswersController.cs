5d67966f3e192e8a2cb2a18b0564ad4600d803c0
Marcio Fernandes <marcio.fernandes@atos.net>
2023-05-01 13:43:12 -0300

    início implementação testAnswer

--- backend/api/Controllers/TestAnswersController.cs
+++ backend/api/Controllers/TestAnswersController.cs
@@ -2,6 +2,9 @@
 using dll.Models;
 using viewmodels;
 using Microsoft.AspNetCore.Mvc;
+using dll.Models.CareerMap;
+using Microsoft.VisualBasic;
+using dll.Models.Form;
 
 namespace api.Controllers
 {
@@ -10,13 +13,21 @@ namespace api.Controllers
     public class TestAnswersController : ControllerBase
     {
         private readonly IConfiguration _configuration;
-        private readonly TestAnswersDAO _testAnswersDAO;
+        private readonly TestAnswersDAO _dao;
         public string ConnectionString { get; set; }
         public TestAnswersController(IConfiguration configuration)
         {
             _configuration = configuration;
             ConnectionString = _configuration.GetConnectionString("AprovAtosConnection");
-            _testAnswersDAO = new TestAnswersDAO(ConnectionString);
+            _dao = new TestAnswersDAO(ConnectionString);
         }
+
+        //[HttpPost]
+        //[Route("insert")]
+        //public IActionResult PostCareerMap(List<MTestAnswer> answers)
+        //{
+        //    _dao.InsertCareerMap(careerMap);
+        //    return Ok(new { message = "The career map was successfully registered." });
+        //}
     }
 }
