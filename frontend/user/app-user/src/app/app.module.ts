63342b3a45fc3c46e11c05904982abffbe6a4aff
Lucas Neves <lucasbarbozaneves@gmail.com>
2023-05-02 10:11:56 -0300

    Criação da Modal responsável por listar os testes de um requesito específico.

--- frontend/user/app-user/src/app/app.module.ts
+++ frontend/user/app-user/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
@@ -18,7 +18,8 @@ import { CareerMapComponent } from './components/career-map/career-map.component
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/user/app-user/src/app/components/career-map/career-map.component.ts
+++ frontend/user/app-user/src/app/components/career-map/career-map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CompanyPositionsService } from 'src/app/services/company-position/company-positions.service';
 import { RequirementsService } from 'src/app/services/requirement/requirements.service';
+import { TestsService } from 'src/app/services/test/tests.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-career-map',
@@ -12,10 +14,16 @@ export class CareerMapComponent implements OnInit {
   companyPositions: any[] = [];
   selectedPosition: any;
   requirements: any[] = [];
+  tests: any[] = [];
+
+  title = 'appBootstrap';  
+  closeResult: string = '';
 
   constructor(
     private companyPositionsService: CompanyPositionsService,
-    private requirementsService: RequirementsService
+    private requirementsService: RequirementsService,
+    private testsService: TestsService,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit() {
@@ -28,5 +36,29 @@ export class CareerMapComponent implements OnInit {
     this.requirementsService.getRequirements(companyPositionId).subscribe(data => {
       this.requirements = data.requirements;
     });
+  }
+
+  selectRequirements(requirementId: number){
+    this.testsService.getTests(requirementId).subscribe(data => {
+      this.tests = data.tests;
+    });
+  }
+
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
