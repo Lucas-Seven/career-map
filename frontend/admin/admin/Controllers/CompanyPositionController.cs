eef16265a8b2fd0c9adc61802585dba93eff40f2
Marcio Fernandes <marcio.fernandes@atos.net>
2023-05-01 21:54:40 -0300

    requirement with companyPosition

--- frontend/admin/admin/Controllers/CompanyPositionController.cs
+++ frontend/admin/admin/Controllers/CompanyPositionController.cs
@@ -42,5 +42,66 @@ namespace admin.Controllers
                 return View();
             }
         }
+
+        //        [HttpGet]
+        //public ActionResult AssociateRequirements(int careerMapId, int companyPositionId)
+        //{
+        //    var availableRequirements = new List<CompanyPositionInfo>();
+        //    var unavailableRequirements = new List<CompanyPositionInfo>();
+        //    var init = new CompanyPositionListResponse();
+        //    var position = _service.GetAllPositions().Find(x => x.CompanyPositionId == companyPositionId && x.ParentCareerMapVm.CareerMapId == careerMapId);
+
+        //    if (position != null)
+        //    {
+        //        try
+        //        {
+        //            var _serviceCompanyPosition = new CompanyPositionService();
+
+        //            var existingPositions = _service.LoadCompanyPositionsFromCareerMapId(careerMapId);
+        //            var all = _serviceCompanyPosition.GetAllPositions();
+
+        //            foreach (var pos in all)
+        //            {
+        //                var info = new CompanyPositionInfo()
+        //                {
+        //                    CompanyPositionId = pos.CompanyPositionId,
+        //                    CompanyPositionName = pos.CompanyPositionName
+        //                };
+
+        //                if (existingPositions.CompanyPositionResponseList.Find(x => x.CompanyPositionInfo.CompanyPositionId == pos.CompanyPositionId) != null)
+        //                {
+        //                    unavailableRequirements.Add(info);
+        //                }
+        //                else
+        //                {
+        //                    availableRequirements.Add(info);
+        //                }
+        //            }
+
+        //        }
+        //        catch (Exception) { /* throw; */ }
+        //    }
+
+        //    ViewBag.AvailablePositions = availableRequirements;
+        //    ViewBag.UnavailablePositions = unavailableRequirements;
+
+        //    init.CareerMapResponse = position == null ? new CareerMapResponse() : position;
+
+        //    return View(init);
+        //}
+
+        //[HttpPost]
+        //public ActionResult AssociatePositions(CompanyPositionListResponse positions)
+        //{
+        //    var ret = _service.AssociatePositions(positions);
+        //    if (ret)
+        //    {
+        //        return RedirectToAction("CompanyPositionsCareerMapDetail", new { careerMapId = positions.CareerMapResponse.CareerMapId });
+        //    }
+        //    else
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
